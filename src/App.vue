5207ad1ddb1f04febddff577045043813975b30e
Alex Lin <unclelin2020@gmail.com>
2022-05-28 22:21:25 +0800

    fix: set font-size

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <v-app>
     <v-main class="background">
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style>
+@import "./assets/css/style.css";
+</style>
