a90ec17dcf9ad93ff912dc622556ded9747377bd
Alex Lin <unclelin2020@gmail.com>
2022-05-29 11:42:39 +0800

    fix: init Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,14 @@
 <template>
-  <div class="home">
-    <h1>h1</h1>
-    <h2>h2</h2>
-    <h3>h3</h3>
-    <h4>h4</h4>
-    <h5>h5</h5>
-    <h6>h6</h6>
-    <p>
-      p
-      <span>span</span>
-    </p>
-    <small>small</small>
-    <h1>台北黑體</h1>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  data() {
+    return {};
+  }
 };
 </script>
+
+<style scoped>
+</style>
