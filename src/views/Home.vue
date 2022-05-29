c55cd51d45181cff6e367a95a02f60794171647b
Alex Lin <unclelin2020@gmail.com>
2022-05-29 16:37:42 +0800

    fix: beautify portal card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,17 @@
     </v-row>
     <v-row>
       <v-col md="4" cols="12">
-        <v-card height="150px">
-          <div>
+        <v-card height="150px" class="d-flex flex-column px-5 py-3 rounded-lg">
+          <div class="d-flex align-center flex-grow-0">
             <h3 class="primary--text">
               <strong>王小明</strong>
             </h3>
-            <h4 class="gray--text">男</h4>
+            <h4 class="gray--text ml-auto">男</h4>
           </div>
-          <div>
+          <div class="flex-grow-0">
             <span class="gray--text">2021062011493428</span>
           </div>
-          <div>
+          <div class="d-flex flex-grow-1 align-end">
             <v-icon left color="secondary">mdi-gmail</v-icon>
             <span class="secondary--text">Takmig001@takming.edu.tw</span>
           </div>
