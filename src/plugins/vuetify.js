8ae11b04bc0d1154345ea35ad72ab624a83e914f
Uncle Lin <unclelin2020@gmail.com>
2022-06-01 16:19:24 +0800

    feat: set color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#555555",
+                secondary: "#484848",
+                gray: "#8d8d8d",
+                white: "#fffff",
+                background: "#efefef"
+            }
+        }
+    }
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>123456789</div>
+  <div></div>
 </template>
 
 <script>
