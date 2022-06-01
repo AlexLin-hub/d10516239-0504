6ebae385bf1fb606f02f59be828c8b21bead9f30
Uncle Lin <unclelin2020@gmail.com>
2022-06-01 16:55:21 +0800

    fix: init HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,5 @@
 <template>
-  <div>
-    <h1>H1</h1>
-    <h2>H2</h2>
-    <h3>H3</h3>
-    <h4>H4</h4>
-    <h5>H5</h5>
-    <h6>H6</h6>
-    <p>p</p>
-    <span>span</span>
-    <small>small</small>
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -19,3 +9,7 @@ export default {
   },
 };
 </script>
+
+
+<style>
+</style>
