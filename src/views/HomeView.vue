6c674d306bb83e57d62b281578e55beaeba5c3fc
Uncle Lin <unclelin2020@gmail.com>
2022-06-01 17:04:04 +0800

    fix: init portal card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,26 @@
         <h1 class="text-center py-10">人員清單</h1>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col md="4" cols="12">
+        <v-card height="150px">
+          <div>
+            <h3 class="primary--text"><strong>王小明</strong></h3>
+            <h4 class="gray--text">男</h4>
+          </div>
+
+          <div>
+            <span class="gray--text"> 202120120120120 </span>
+          </div>
+
+          <div>
+            <v-icon left color="secondary"> mdi-gmail </v-icon>
+            <span class="secondary--text"> Takming001@gs.takming.edu.tw </span>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
